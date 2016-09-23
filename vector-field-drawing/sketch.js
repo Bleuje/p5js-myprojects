8795893b2a44f64006599e928d9ba1f825dabf09
Etienne JACOB <etin.jacob@gmail.com>
2016-09-23 20:50:00 +0200

    size fix

--- vector-field-drawing/sketch.js
+++ vector-field-drawing/sketch.js
@@ -45,7 +45,7 @@ var csize = 0;
 var cnv;
 
 function setup() {
-  cnv = createCanvas(max(50,windowWidth-225), HEI);
+  cnv = createCanvas(max(50,windowWidth-150), HEI);
   cnv.style("border:1px solid #000000;");
   cnv.parent('canvas');
   cnv.style('z-index: 1')
@@ -519,7 +519,7 @@ function mySelectEvent5() {
 function resizec() {
   csize = (csize + 1)%2;
   cnv.remove();
-  cnv = createCanvas(max(50,windowWidth-225), HEI + csize*200);
+  cnv = createCanvas(max(50,windowWidth-150), HEI + csize*200);
   cnv.style("border:1px solid #000000;");
   cnv.parent('canvas');
   cnv.style('z-index: 1');
